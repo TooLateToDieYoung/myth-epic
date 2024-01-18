df0194e9ee062709728e84eabdb73f6173e2ee01
shrimp <tolatetodieyoung1204@gmail.com>
2024-01-18 22:06:53 +0800

    [M] Try to use vue-router API and compare these two methods

--- web-blog/vite-project/src/components/Lyrics.vue
+++ web-blog/vite-project/src/components/Lyrics.vue
@@ -1,9 +1,20 @@
 <script setup>
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute();
+const name = ref(route.params.name);
 
 </script>
 
 <template>
-    <div>{{ $route.params.name }}</div>
+    <div>
+        <!-- get from setup ref() -->
+        <div>{{ name }}</div>
+
+        <!-- get from this #app instance => vue.use(route) => vue.$route.params -->
+        <div>{{ $route.params.name }}</div>
+    </div>
 </template>
 
 <style scoped>
