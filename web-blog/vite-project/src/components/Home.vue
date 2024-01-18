6f1f79aea60bb3ac769ba864ab25accb128fbb42
shrimp <tolatetodieyoung1204@gmail.com>
2024-01-18 21:31:27 +0800

    [M] Try to use Vue3 API

--- web-blog/vite-project/src/components/Home.vue
+++ web-blog/vite-project/src/components/Home.vue
@@ -1,16 +1,29 @@
 <script setup>
+import { ref, onBeforeMount } from 'vue'
 
+const menu = ref({});
+const clickHandler = ref( async function () {
+    await Promise.all([
+        setTimeout(console.log, 1000, '>'),
+        setTimeout(console.log, 2000, '<')
+    ]);
+
+    console.log('mythepic');
+});
+
+onBeforeMount( async function () { 
+    await setTimeout(console.log, 5000, 'test');
+    menu.value['myth'] = true;
+    menu.value['epic'] = 6666;
+});
 </script>
 
 <template>
-    <div>Home</div>
+    <div @click="clickHandler">
+        Home
+        <nav v-for="(val, key, idx) in menu" :key="key">[{{ idx }}] {{ key }} : {{ val }}</nav>
+    </div>
 </template>
-
-<script>
-export default {
-
-}
-</script>
 
 <style scoped>
 
